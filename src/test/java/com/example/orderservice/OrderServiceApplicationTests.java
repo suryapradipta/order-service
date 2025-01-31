042e9b31bd95603517d2c43cbe7e51ff52a6127c
I Nyoman Surya Pradipta <suryapradipta8@gmail.com>
2025-02-01 02:47:48 +0800

    [Order] Test Containers

--- src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
@@ -1,15 +1,54 @@
 package com.example.orderservice;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:17.2");
 
+    @LocalServerPort
+    private Integer port;
+
+    static {
+        postgreSQLContainer.start();
+    }
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void shouldPlaceOrder() {
+        String requestBody = """
+                {
+                      "orderNumber": "12345",
+                      "skuCode": "SKU001",
+                      "price": 100.00,
+                      "quantity": 2
+                }
+                """;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(201)
+                .body(Matchers.equalTo("order placed successfully"));
+
+    }
 }
