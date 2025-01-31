6e59c591547fb54425e0d30d03a311a82d48bff9
I Nyoman Surya Pradipta <suryapradipta8@gmail.com>
2025-02-01 01:55:57 +0800

    [Order] REST and Flayway

---
+++ src/main/java/com/example/orderservice/model/Order.java
@@ -0,0 +1,26 @@
+package com.example.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
---
+++ src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,29 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = Order.builder()
+                .orderNumber(UUID.randomUUID().toString())
+                .skuCode(orderRequest.skuCode())
+                .price(orderRequest.price())
+                .quantity(orderRequest.quantity())
+                .build();
+
+        this.orderRepository.save(order);
+
+    }
+}
