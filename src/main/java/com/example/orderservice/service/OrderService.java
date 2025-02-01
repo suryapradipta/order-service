d37ea3409cf10528b8bd69e779665f30df6ff680
I Nyoman Surya Pradipta <suryapradipta8@gmail.com>
2025-02-01 22:10:09 +0800

    [order] synchronous using spring feign

--- src/main/java/com/example/orderservice/service/OrderService.java
+++ src/main/java/com/example/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.orderservice.service;
 
+import com.example.orderservice.client.InventoryClient;
 import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.repository.OrderRepository;
@@ -13,9 +14,17 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
 
     public void placeOrder(OrderRequest orderRequest) {
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (!isProductInStock) {
+            throw new IllegalArgumentException("Product with code " + orderRequest.skuCode() + " is not in stock");
+        }
+
+
         Order order = Order.builder()
                 .orderNumber(UUID.randomUUID().toString())
                 .skuCode(orderRequest.skuCode())
